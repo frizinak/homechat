916f3be43f077da3e14a94f33d16d3fd1dfa5d18
Kobe Lipkens <kobelipkens@gmail.com>
2021-01-23 20:23:39 +0100

    autocompletion tweak

--- cmd/client/completion.go
+++ cmd/client/completion.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
@@ -16,28 +17,56 @@ func complete(input, prefix string, list []string, excludes map[string]struct{})
 		return ""
 	}
 
-	found := ""
-	foundC := 0
+	found := make([]string, 0)
 	for _, n := range list {
 		if _, ok := excludes[n]; ok {
 			continue
 		}
 
 		if strings.HasPrefix(n, l[len(prefix):]) {
-			found = n
-			if foundC++; foundC > 1 {
-				return ""
-			}
+			found = append(found, n)
 		}
 	}
 
-	if foundC != 1 {
+	if len(found) == 0 {
 		return ""
 	}
+
+	var match string
+	var shortest string
+	sort.Slice(found, func(i, j int) bool {
+		return len(found[i]) < len(found[j])
+	})
+
+	shortest, found = found[0], found[1:]
+	for i := len(shortest); i >= 0; i-- {
+		matchAll := true
+		for _, f := range found {
+			if f[:i] != shortest[:i] {
+				matchAll = false
+				break
+			}
+		}
+		if matchAll {
+			match = shortest[:i]
+			break
+		}
+	}
+
+	if match == "" {
+		return ""
+	}
+
+	var suffix string
+	if len(found) == 0 {
+		suffix = " "
+	}
+
 	i := fmt.Sprintf(
-		"%s%s ",
+		"%s%s%s",
 		prefix,
-		found,
+		match,
+		suffix,
 	)
 	if len(p) > 1 {
 		i = fmt.Sprintf(
