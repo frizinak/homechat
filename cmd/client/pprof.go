d039f37475e71d6e4a3b49a3f1679190058f21aa
Kobe Lipkens <kobelipkens@gmail.com>
2021-02-02 10:59:00 +0100

    client pprof

---
+++ cmd/client/pprof.go
@@ -0,0 +1,18 @@
+// +build pprof
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+)
+
+func init() {
+	fmt.Println("pprof build! (:6061")
+	go func() {
+		if err := http.ListenAndServe(":6061", nil); err != nil {
+			fmt.Printf("failed to start pprof http server: %s\n", err)
+		}
+	}()
+}
